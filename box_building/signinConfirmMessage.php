7f659931f5cd3d67444abc95407b5a96cffd5149
unknown <pedroberbel.br@gmail.com>
2018-04-16 15:02:23 -0300

    Adicionadas as telas de cadastro de informações pessoais e físicas

--- box_building/signinConfirmMessage.php
+++ box_building/signinConfirmMessage.php
@@ -68,63 +68,20 @@
 
         <!-- REGISTRATION FORM -->
         <div class="text-center" style="padding:50px 0">
-        	<div class="logo">Criar Conta</div>
-        	<!-- Main Form -->
-        	<div class="login-form-1">
+        	<div class="logo">Confirmar Email</div>
 
-        		<form id="register-form" class="text-left" method="GET" action="signinConfirmMessage.php">
-        			<div class="login-form-main-message"></div>
-        			<div class="main-login-form">
-        				<div class="login-group">
-        					<div class="form-group">
-        					<!--	<label for="reg_username" class="sr-only">Email address</label>
-        						<input type="text" class="form-control" id="reg_username" name="reg_username" placeholder="Usuário"> -->
-                      <label for="reg_email" class="sr-only">Email</label>
-                      <input type="text" class="form-control" id="reg_email" name="reg_email" placeholder="email">
+        <p>Um email foi enviado para <?php echo $_GET["reg_email"]; ?>, insira o código de validação no campo abaixo: </p>
 
-        					</div>
-        					<div class="form-group">
-        						<label for="reg_password" class="sr-only">Password</label>
-        						<input type="password" class="form-control" id="reg_password" name="reg_password" placeholder="Senha">
-        					</div>
-        					<div class="form-group">
-        						<label for="reg_password_confirm" class="sr-only">Password Confirm</label>
-        						<input type="password" class="form-control" id="reg_password_confirm" name="reg_password_confirm" placeholder="Confirmar Senha">
-        					</div>
-
-        				<!--	<div class="form-group">
-        						<label for="reg_email" class="sr-only">Email</label>
-        						<input type="text" class="form-control" id="reg_email" name="reg_email" placeholder="email">
-        					</div> -->
-              <!--
-        					<div class="form-group">
-        						<label for="reg_fullname" class="sr-only">Full Name</label>
-        						<input type="text" class="form-control" id="reg_fullname" name="reg_fullname" placeholder="Nome Completo">
-        					</div>
-
-        					<div class="form-group login-group-checkbox">
-        						<input type="radio" class="" name="reg_gender" id="male" placeholder="username">
-        						<label for="male">Masculino</label>
-
-        						<input type="radio" class="" name="reg_gender" id="female" placeholder="username">
-        						<label for="female">Feminino</label>
-        					</div>
-                -->
-        					<div class="form-group login-group-checkbox">
-        						<input type="checkbox" class="" id="reg_agree" name="reg_agree">
-        						<label for="reg_agree">Eu concordo com os <a href="#">termos</a></label>
-        					</div>
-        				</div>
-
-        				<button type="submit" class="login-button"><i class="fa fa-chevron-right"></i></button>
-        			</div>
-        			<div class="etc-login-form">
-        				<p>Já é cadastrado? <a href="login.html">Entre Aqui</a></p>
-        			</div>
-        		</form>
-        	</div>
-        	<!-- end:Main Form -->
-        </div>
+        <form action="signinPersonalInformation.html" >
+          Código de Verificação:<br>
+          <input type="text" name="validationCode" >
+          <br>
+          Reenviar
+          <br><br>
+          <input type="submit" value="Submit">
+        </form>
+      </div>
+    </div>
 
 
         <!-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11     -->
